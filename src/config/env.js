95da87c1c6cb9a4affe2578947cbc713b48dde78
aryanboii <arya94189@gmail.com>
2026-08-31 11:43:19 +0530

    chore: initialize ShopSphere API

---
+++ src/config/env.js
@@ -0,0 +1,17 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const env = {
+    port: process.env.PORT || 5000,
+
+    db: {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 5432,
+        name: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+    },
+};
+
+module.exports = env;
